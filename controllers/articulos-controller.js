5688f678ec1fd1381bdd71dc31bcc5f3539bc696
Juan <sebastianorduz97@outlook.com>
2020-12-16 15:20:27 -0500

    Adding controller, middleware and services

---
+++ controllers/articulos-controller.js
@@ -0,0 +1,96 @@
+const models = require('../models');
+
+module.exports = {
+    add: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.create(req.body);
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    query: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findOne({ where: { id: req.query.id } });
+            if (!reg) {
+                res.status(404).send({
+                    message: 'El registro no existe'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    list: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findAll();
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    remove: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.destroy({
+                where: {
+                    id:
+                        req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    update: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.update({
+                nombre: req.body.nombre, descripcion:
+                    req.body.descripcion
+            }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    activate: async (req, res, next) => {
+        try {
+            console.log(req.body._id);
+            const reg = await models.Articulo.update({ estado: 1 }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    deactivate: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.update({ estado: 0 }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    }
+}
