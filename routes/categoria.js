b591ce9bfe8e923f1e1e9e410d2f49be76cc2dd1
Juan <sebastianorduz97@outlook.com>
2020-12-17 07:47:18 -0500

    Adding middleware to authentication

--- routes/categoria.js
+++ routes/categoria.js
@@ -5,13 +5,13 @@ const auth = require('../middlewares/auth');
 const router = routerx();
 
 
-router.post('/add', categoriaController.add);
-router.get('/query', categoriaController.query);
-router.get('/list', categoriaController.list);
-router.put('/update', categoriaController.update);
-router.delete('/remove', categoriaController.remove);
-router.put('/activate', categoriaController.activate);
-router.put('/deactivate', categoriaController.deactivate);
+router.post('/add',auth.verifyUsuario, categoriaController.add);
+router.get('/query',auth.verifyUsuario, categoriaController.query);
+router.get('/list',auth.verifyUsuario, categoriaController.list);
+router.put('/update',auth.verifyUsuario, categoriaController.update);
+router.delete('/remove',auth.verifyUsuario, categoriaController.remove);
+router.put('/activate',auth.verifyUsuario, categoriaController.activate);
+router.put('/deactivate',auth.verifyUsuario, categoriaController.deactivate);
 
 
 module.exports = router;
